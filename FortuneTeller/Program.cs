f85d8dc8d2ea7d08461f4817f6493cd807ed4d57
Katherine Beam <k.beam@umiami.edu>
2016-03-18 11:09:41 -0400

    Created methods for quitting and restarting

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,30 +20,35 @@ namespace FortuneTeller
                 Console.WriteLine("Please enter your first name:");
                 string firstName = Console.ReadLine();
                 Quit(firstName);
-                //if(firstName.ToUpper() == "QUIT")
-                //{
-                //    Environment.Exit(0);
-                //}
+                Restart(firstName);
                 char firstNameLetter = char.ToUpper(firstName[0]);
                 Console.WriteLine("Ah yes, " + firstName + ". Now please enter your last name my child:");
                 string lastName = Console.ReadLine();
+                Quit(lastName);
+                Restart(lastName);
                 Console.WriteLine("Please enter your current age:");
-                int age = int.Parse(Console.ReadLine());
+                string age = Console.ReadLine();
+                Quit(age);
+                Restart(age);
+                int ageNum = int.Parse(age);
                 Console.WriteLine("In which month were you born?");
                 string birthMonth = Console.ReadLine().ToUpper();
-
+                Quit(birthMonth);
+                Restart(birthMonth);
                 string favoriteColor = "";
                 if (favoriteColor.ToUpper() == "QUIT")
                 {
                     break;
                 }
                 else
-                { 
+                {
                     do
                     {
                         Console.WriteLine("Please enter your favorite ROYGBIV color:");
                         Console.WriteLine("(Not sure what ROYGBIV means? Type \"Help\".)");
                         favoriteColor = Console.ReadLine().ToUpper();
+                        Quit(favoriteColor);
+                        Restart(favoriteColor);
                         if (favoriteColor == "HELP")
                         {
                             Console.WriteLine("Red\nOrange\nYellow\nGreen\nBlue\nIndigo\nViolet");
@@ -52,25 +58,26 @@ namespace FortuneTeller
                 }
 
                 Console.WriteLine("How many siblings do you have?");
-                int siblings = int.Parse(Console.ReadLine());
-
+                string siblings = Console.ReadLine();
+                Quit(siblings);
+                Restart(siblings);
+                int siblingNum = int.Parse(siblings);
 
                 //PART TWO
-
                 //Retirement
                 int retirement = 0;
-                if (age % 2 != 0)
+                if (ageNum % 2 != 0)
                 {
                     retirement = 1;
                 }
-                else if (age % 2 == 0)
+                else if (ageNum % 2 == 0)
                 {
                     retirement = 400;
                 }
 
                 //Siblings
                 string location = "";
-                switch (siblings)
+                switch (siblingNum)
                 {
                     case 0:
                         location = " in the closet";
@@ -150,20 +157,28 @@ namespace FortuneTeller
 
                 Console.WriteLine("Want to play again? (y/n)");
                 playAgain = Console.ReadLine().ToUpper();
+                Quit(playAgain);
             }
             while (playAgain == "Y");
         }
         static void Quit(string userReply)
         {
             userReply = userReply.ToUpper();
-            string quitMessage = "";
             if (userReply == "QUIT")
             {
+                Console.WriteLine("Nobody likes a quitter...");
                 Environment.Exit(0);
-                quitMessage = "Nobody likes a quitter...";
             }
+        }
 
-            Console.WriteLine(quitMessage);
+        static void Restart(string userReply)
+        {
+            if (userReply == "Restart")
+            {
+                var fileName = Assembly.GetExecutingAssembly().Location;
+                System.Diagnostics.Process.Start(fileName);
+                Environment.Exit(0);
+            }
 
         }
     }
